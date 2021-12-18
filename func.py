0a1bba7b22601bf4c668cf88cedadf131a23483c
hanerdong <erdonghan.com>
2021-12-18 18:53:45 +0800

    add func.py

---
+++ func.py
@@ -0,0 +1,12 @@
+# ==================================================================================================================== #
+#                                                         -*-coding: UTF-8 -*-
+#     * * * * * * * * * * * * * * * * *                       Time : 2021/12/18 18:52
+#     * COPYRIGHT NOTICE              *                     Author : HAN Erdong
+#     * Copyright © 2021 Han Erdong   *                   FileName : func.py
+#     * All rights reserved           *                ProjectName : Github_try
+#     * * * * * * * * * * * * * * * * *                   Software : PyCharm
+#                                                   Python Version : Python 3.8                                                         
+#                                                         Function : (Write the function of this *.py file here)
+# ==================================================================================================================== #
+def func():
+    return 1 + 2
